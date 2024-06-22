511933f07ed07b386f5aa026f39e66774afcc4b6
Bhavesh Ramburn <nitrogen@gmail.com>
2024-06-22 08:58:44 +0100

    Refactor TASKPRED class constructor to remove unnecessary predecessor and successor parameters.

--- xerparser/schemas/taskpred.py
+++ xerparser/schemas/taskpred.py
@@ -4,16 +4,14 @@
 from datetime import datetime
 import pandas as pd
 
-from xerparser.schemas.task import TASK
 from xerparser.src.validators import optional_int, optional_date, int_or_zero, optional_str
-
 
 class TASKPRED:
     """
     A class to represent a relationship between two activities.
     """
 
-    def __init__(self, row: pd.Series, predecessor: TASK, successor: TASK) -> None:
+    def __init__(self, row: pd.Series) -> None:
         self.uid: str = row['task_pred_id']
         self.task_id: str = row['task_id']
         self.pred_task_id: str = row['pred_task_id']
@@ -25,32 +23,30 @@ class TASKPRED:
         self.aref: datetime | None = optional_date(row['aref'])
         self.arls: datetime | None = optional_date(row['arls'])
         self.comments: str | None = optional_str(row['comments'])
-        self.predecessor: TASK = predecessor
-        self.successor: TASK = successor
 
     def __eq__(self, __o: "TASKPRED") -> bool:
         return (
-            self.predecessor == __o.predecessor
-            and self.successor == __o.successor
+            self.task_id == __o.task_id
+            and self.pred_task_id == __o.pred_task_id
             and self.link == __o.link
         )
 
     def __gt__(self, __o: "TASKPRED") -> bool:
-        if self.predecessor == __o.predecessor:
-            if self.successor == __o.successor:
+        if self.task_id == __o.task_id:
+            if self.pred_task_id == __o.pred_task_id:
                 return self.link > __o.link
-            return self.successor > __o.successor
-        return self.predecessor > __o.predecessor
+            return self.pred_task_id > __o.pred_task_id
+        return self.task_id > __o.task_id
 
     def __lt__(self, __o: "TASKPRED") -> bool:
-        if self.predecessor == __o.predecessor:
-            if self.successor == __o.successor:
+        if self.task_id == __o.task_id:
+            if self.pred_task_id == __o.pred_task_id:
                 return self.link < __o.link
-            return self.successor < __o.successor
-        return self.predecessor < __o.predecessor
+            return self.pred_task_id < __o.pred_task_id
+        return self.task_id < __o.task_id
 
     def __hash__(self) -> int:
-        return hash((self.predecessor, self.successor, self.link))
+        return hash((self.task_id, self.pred_task_id, self.link))
 
     @property
     def lag(self) -> int:
