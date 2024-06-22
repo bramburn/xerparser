748fe483530692e6192eff3549d8d02f18359f4d
Bhavesh Ramburn <nitrogen@gmail.com>
2024-06-22 08:12:21 +0100

    Refactor ACTVCODE class constructor and data processing functions for improved efficiency and readability.

--- xerparser/schemas/actvcode.py
+++ xerparser/schemas/actvcode.py
@@ -1,42 +1,35 @@
 # xerparser
 # actvcode.py
 
+import pandas as pd
+# xerparser
+# actvtype.py
 
-from xerparser.schemas._node import Node
-from xerparser.schemas.actvtype import ACTVTYPE
+import pandas as pd
 
 
-class ACTVCODE(Node):
-    """
-    A class to represent an Activity Code Value
-    """
+class ACTVCODE:
+    def __init__(self, row: pd.Series) -> None:
+        self.actv_code_id: str = row['actv_code_id']
+        self.actv_code_type_id: str = row['actv_code_type_id']
+        self.actv_code: str = row['actv_code']
+        self.actv_code_desc: str = row['actv_code_desc']
+        self.is_active: bool = row['is_active']
+        self.max_length: int = row['actv_short_len']
+        self.proj_id: str = row['proj_id']
+        self.scope: str = _check_scope(row['actv_code_type_scope'])
+        self.seq_num: int | None = None if pd.isna(row['seq_num']) else int(row['seq_num'])
 
-    def __init__(self, code_type: ACTVTYPE, **data: str) -> None:
-        super().__init__(
-            data["actv_code_id"],
-            data["short_name"],
-            data["actv_code_name"],
-            data["parent_actv_code_id"],
-        )
-        self.actv_code_type_id: str = data["actv_code_type_id"]
-        """Foreign Key to Activity Code Type"""
-        self.seq_num: int = int(data["seq_num"])
-        """Sort Order"""
-        self.code_type: ACTVTYPE = self._valid_actvtype(code_type)
-        """Activity Code Type"""
 
-    def __eq__(self, __o: "ACTVCODE") -> bool:
-        return self.full_code == __o.full_code and self.code_type == __o.code_type
+def _check_scope(value: str) -> str:
+    if not value.startswith("AS_"):
+        raise ValueError(f"Expected 'AS_Project' or 'AS_Global', got {value}")
+    return value[3:]
 
-    def __hash__(self) -> int:
-        return hash((self.full_code, self.code_type))
 
-    def _valid_actvtype(self, value: ACTVTYPE) -> ACTVTYPE:
-        """Validate Activity Code Type"""
-        if not isinstance(value, ACTVTYPE):
-            raise TypeError(f"Expected <class ACTVTYPE>; got {type(value)}")
-        if value.uid != self.actv_code_type_id:
-            raise ValueError(
-                f"{value.uid} does not match act_code_type_id {self.actv_code_type_id}"
-            )
-        return value
+def _process_actvcode_data(actvcode_df: pd.DataFrame) -> dict[str, ACTVCODE]:
+    actvcode_values = {}
+    for _, row in actvcode_df.iterrows():
+        actvcode = ACTVCODE(row)
+        actvcode_values[actvcode.actv_code_id] = actvcode
+    return actvcode_values
